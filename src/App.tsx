ac72af287bcdc7355487264aee0073dbaaa05990
UgneM <ugnem@wix.com>
2024-08-01 23:12:02 +0300

    Minor changes and suggestions

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import { useState } from 'react';
 import { Modal } from './Modal/Modal';
 function App() {
   const [isOpen, setIsOpen] = useState(false);
+  // const [startInput, setStartInput] = useState('');
+  // const [endInput, setEndInput] = useState('');
+  // const [dateInput, setDateInput] = useState('');
   function openCloseModal() {
     setIsOpen(!isOpen);
   }
@@ -13,7 +16,14 @@ function App() {
     <>
       <Header />
       <MainScreen callback={openCloseModal} />
-      {isOpen && <Modal callback={openCloseModal} />}
+      {isOpen && (
+        <Modal
+          callback={openCloseModal}
+          // passStartInput={setStartInput}
+          // passEndInput={setEndInput}
+          // passDateInput={setDateInput}
+        />
+      )}
     </>
   );
 }
